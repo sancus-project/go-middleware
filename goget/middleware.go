a7d2be530156601f4d22303cc13d96aff62bc7d0
Alejandro Mery <amery@geeks.cl>
2021-04-11 17:17:30 +0000

    goget: implement Middleware views
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

--- goget/middleware.go
+++ goget/middleware.go
@@ -5,30 +5,47 @@ import (
 	"net/http"
 )
 
-func MiddlewareFromFile(fn string, renderer Renderer) func(http.Handler) http.Handler {
+// Middleware view
+type Middleware struct {
+	Renderer          Renderer // renders go-import html
+	OnlyGoGet         bool     // only render go-import html if ?go-get=1
+	RedirectToDoc     bool     // if package exists, but ?go-get=1 isn't give, redirect to documentation
+	RedirectToSources bool     // if package exists, but ?go-get=1 isn't give, redirect to sources
+}
+
+// Middleware constructor from .ini file
+func MiddlewareFromFile(fn string, options ...MiddlewareOption) func(http.Handler) http.Handler {
 	m, err := PackagesFromFile(fn)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	return m.Middleware(renderer)
+	return m.NewMiddleware(options...)
 }
 
-func (m *Packages) Middleware(renderer Renderer) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return m.Handler(next, renderer)
+// Middleware constructor from Config
+func (c *Config) NewMiddleware(options ...MiddlewareOption) func(http.Handler) http.Handler {
+	v := NewMiddleware(c.Renderer, options...)
+	fn := func(next http.Handler) http.Handler {
+		return v.NewHandler(next, c.config.Package)
 	}
+
+	return fn
 }
 
-func (c *Config) Middleware(next http.Handler) http.Handler {
-	return c.config.Package.Handler(next, c.Renderer)
-}
-
-func (m *Packages) Handler(next http.Handler, renderer Renderer) http.Handler {
-	if renderer == nil {
-		renderer = DefaultRenderer()
+// Middleware constructor from Packages
+func (m Packages) NewMiddleware(options ...MiddlewareOption) func(http.Handler) http.Handler {
+	v := NewMiddleware(nil, options...)
+	fn := func(next http.Handler) http.Handler {
+		return v.NewHandler(next, m)
 	}
+
+	return fn
+}
+
+// Handler
+func (v *Middleware) NewHandler(next http.Handler, m Packages) http.Handler {
 
 	if next == nil {
 		next = http.NotFoundHandler()
@@ -36,17 +53,58 @@ func (m *Packages) Handler(next http.Handler, renderer Renderer) http.Handler {
 
 	fn := func(w http.ResponseWriter, r *http.Request) {
 
-		pkg := m.Get(r)
-		if pkg != nil {
-			err := renderer(w, r, pkg)
-			if err != nil {
-				log.Fatal(err)
+		if pkg, goget := v.Get(m, r); pkg != nil {
+
+			if goget {
+				// render go-import page
+				err := v.Renderer(w, r, pkg)
+				if err != nil {
+					log.Fatal(err)
+				}
+				return
+			} else if v.RedirectToDoc {
+				// redirect to doc
+				url := "https://pkg.go.dev/" + r.URL.Path
+				http.Redirect(w, r, url, 407)
+				return
+			} else if v.RedirectToSources {
+				// redirect to repo
+				url := pkg.Repository
+				http.Redirect(w, r, url, 407)
+				return
 			}
-			return
 		}
 
+		// next
 		next.ServeHTTP(w, r)
 	}
 
 	return http.HandlerFunc(fn)
+}
+
+func (_ Middleware) isGoGet(query map[string][]string) bool {
+	// exactly ?go-get=1
+	if len(query) == 1 {
+		if m, ok := query["go-get"]; ok {
+			if len(m) == 1 && m[0] == "1" {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (v *Middleware) Get(m Packages, r *http.Request) (pkg *View, goget bool) {
+
+	pkg = m.Get(r)
+	if pkg == nil {
+		// unknown
+	} else if !v.OnlyGoGet {
+		// render go-import page to all matches
+		goget = true
+	} else {
+		// render go-import page only if ?go-get=1 is passed. exactly.
+		goget = v.isGoGet(r.URL.Query())
+	}
+	return
 }
---
+++ goget/options.go
@@ -0,0 +1,61 @@
+package goget
+
+// Middleware Options
+type MiddlewareOption interface {
+	isMiddlewareOption() MiddlewareOption
+}
+
+type middlewareOption struct{}
+
+func (o middlewareOption) isMiddlewareOption() MiddlewareOption {
+	return o
+}
+
+type OnlyGoGet struct {
+	middlewareOption
+}
+
+type SetRenderer struct {
+	middlewareOption
+	Renderer Renderer
+}
+
+type RedirectToDoc struct {
+	middlewareOption
+}
+
+type RedirectToSources struct {
+	middlewareOption
+}
+
+func NewMiddleware(renderer Renderer, options ...MiddlewareOption) Middleware {
+
+	v := Middleware{}
+
+	for _, o := range options {
+		if _, ok := o.(OnlyGoGet); ok {
+			v.OnlyGoGet = true
+		}
+		if _, ok := o.(RedirectToDoc); ok {
+			v.RedirectToDoc = true
+			v.RedirectToSources = false
+		}
+		if _, ok := o.(RedirectToSources); ok {
+			v.RedirectToDoc = false
+			v.RedirectToSources = true
+		}
+		if r, ok := o.(SetRenderer); ok {
+			v.Renderer = renderer
+		}
+	}
+
+	if v.Renderer == nil {
+		if renderer == nil {
+			renderer = DefaultRenderer()
+		}
+
+		v.Renderer = renderer
+	}
+
+	return v
+}
