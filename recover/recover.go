767bb10bf650e24cd4118a394313f35048b4e838
Alejandro Mery <amery@jpi.io>
2021-05-03 21:02:40 +0000

    recover: moved to go.sancus.dev/web/middleware
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- recover/recover.go
+++ recover/recover.go
@@ -1,27 +1,10 @@
 package recover
 
 import (
-	"net/http"
-
 	"go.sancus.dev/web"
-	"go.sancus.dev/web/errors"
+	"go.sancus.dev/web/middleware/recover"
 )
 
 func Recover(h web.ErrorHandlerFunc) web.MiddlewareHandlerFunc {
-	if h == nil {
-		h = errors.HandleError
-	}
-
-	return func(next http.Handler) http.Handler {
-		fn := func(w http.ResponseWriter, r *http.Request) {
-			defer func() {
-				if err := errors.Recover(); err != nil {
-					h(w, r, err)
-				}
-			}()
-			next.ServeHTTP(w, r)
-		}
-
-		return http.HandlerFunc(fn)
-	}
+	return recover.Recover(h)
 }
