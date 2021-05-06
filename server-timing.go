42ffc9003f5b6b9699adb34592b705e68109eecb
Alejandro Mery <amery@geeks.cl>
2021-05-06 10:35:50 +0100

    server-timing: Introduce ServerTimerMetric() to create a Metric
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

--- server-timing.go
+++ server-timing.go
@@ -4,10 +4,32 @@ import (
 	"net/http"
 
 	"github.com/mitchellh/go-server-timing"
+
+	"go.sancus.dev/web"
 )
 
 // Middleware accessor for github.com/mitchellh/go-server-timing
 // for compatibility with chi.Use()
 func ServerTimer(next http.Handler) http.Handler {
 	return servertiming.Middleware(next, nil)
+}
+
+// Middleware to produce a Server-Timer metric
+func ServerTimerMetric(name string) web.MiddlewareHandlerFunc {
+	if len(name) == 0 {
+		return func(next http.Handler) http.Handler {
+			return next
+		}
+	}
+
+	return func(next http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			if t := servertiming.FromContext(r.Context(); t != nil {
+				defer t.NewMetric(name).Start().Stop()
+			}
+			next.ServeHTTP(w, r)
+		}
+
+		return http.HandlerFunc(fn)
+	}
 }
